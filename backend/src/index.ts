abf9fbeff6424b3042c2753e38e9b76f39979d58
kartavya <kartavyas@gmail.com>
2024-02-12 19:09:18 +1100

    register & login routes with user modal

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import cores from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_URL as string);
 
@@ -10,9 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cores());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "hello from express endpoint!" });
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
