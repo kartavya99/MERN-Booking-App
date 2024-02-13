a778b7c10eec6bbf2ed2f3fdb487c21bc9c7b152
kartavya <kartavyas@gmail.com>
2024-02-13 12:43:46 +1100

    added context & jsonwebtoken for registration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,22 @@ import express, { Request, Response } from "express";
 import cores from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 
 mongoose.connect(process.env.MONGODB_URL as string);
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cores());
+app.use(
+  cores({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
