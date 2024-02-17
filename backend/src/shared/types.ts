71147ba19b396225cba7cb32531fa3094d9f4238
kartavya <kartavyas@gmail.com>
2024-02-17 19:19:06 +1100

    booking details comp added

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -1,3 +1,11 @@
+export type UserType = {
+  _id: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+};
+
 export interface HotelType {
   _id: string;
   userId: string;
