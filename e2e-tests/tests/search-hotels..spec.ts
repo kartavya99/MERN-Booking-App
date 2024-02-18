48df16edb226de8c065840941a737a4fbec032a9
kartavya <kartavyas@gmail.com>
2024-02-18 15:08:40 +1100

    added e2e test

--- e2e-tests/tests/search-hotels..spec.ts
+++ e2e-tests/tests/search-hotels..spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from "@playwright/test";
-import path from "path";
 
 const UI_URL = "http://localhost:5173/";
 test.beforeEach(async ({ page }) => {
@@ -38,3 +37,32 @@ test("should show hotel detail", async ({ page }) => {
   await expect(page).toHaveURL(/detail/);
   await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
 });
+
+// test("should book hotel", async ({ page }) => {
+//   await page.goto(UI_URL);
+
+//   await page.getByPlaceholder("Where are you going").fill("Melbourne");
+
+//   const date = new Date();
+//   date.setDate(date.getDate() + 3);
+//   const formattedDate = date.toISOString().split("T")[0];
+//   await page.getByPlaceholder("Check-out Date").fill(formattedDate);
+
+//   await page.getByRole("button", { name: "Search" }).click();
+
+//   await page.getByText("Melbourne five star").click();
+//   await page.getByRole("button", { name: "Book now" }).click();
+
+//   await expect(page.getByText("Total Cost: $300.00")).toBeVisible();
+
+//   const stripeFrame = page.frameLocator("iframe").first();
+//   await stripeFrame
+//     .locator('[placeholder="Card number"]')
+//     .fill("4242 4242 4242 4242");
+//   await stripeFrame.locator('[placeholder="MM / YY"]').fill("04/30");
+//   await stripeFrame.locator('[placeholder="CVC"]').fill("242");
+//   await stripeFrame.locator('[placeholder="ZIP"]').fill("24225");
+
+//   await page.getByRole("button", { name: "Confirm Booking" }).click();
+//   await expect(page.getByText("Booking Saved!")).toBeVisible();
+// });
